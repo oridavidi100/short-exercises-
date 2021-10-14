0572225de40b306bc9ac7aa4b3e6b560df82f320
oridavidi100 <89572548+oridavidi100@users.noreply.github.com>
2021-10-14 15:02:47 +0300

    Update 01.js

--- Promise async/01.js
+++ Promise async/01.js
@@ -6,34 +6,4 @@
 //     })
     
 
-    //question 1-2
-     let prpr=new Promise((resolve,reject)=>{
-        let result=3;    
-         if (result===3){
-             resolve()
-             
-         }
-         else{ 
-             reject()
-             
-            }
-     })
-     prpr
-     .then(()=>console.log("yes"))
-     .catch(()=>console.log("boo"))
-
-     //question 3
-     function makePromiseWithConstructor(itShouldResolve){
-             return new Promise((resolve,reject)=>{
-             if (itShouldResolve){
-                 resolve()
-             }
-             else{
-                 reject
-             }
-      }) }
-     
-    makePromiseWithConstructor("")
-    .then(()=>console.log("oo"))
-    .catch(()=>console.log("not"))
-     
+  
