33edfe3ec4eb5b63dd5ceef088c62ba1098b31bd
ori davidi <oridavidi100@gmail.com>
2021-11-21 13:21:58 +0200

    hotelgallery

---
+++ 0.20reactContextRouter/hotels/src/components/HotelsGallery.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+const hotels = require('../hotels');
+export class HotelsGallery extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default HotelsGallery;
